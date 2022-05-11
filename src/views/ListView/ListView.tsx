d10333b004a1cd85a178e7c3e1b0ac6c6262472a
Josh Brown <joshuabrown@hey.com>
2022-05-10 20:53:31 -0600

    table responsive fix

--- src/views/ListView/ListView.tsx
+++ src/views/ListView/ListView.tsx
@@ -8,6 +8,8 @@ import {
   useColorModeValue,
   CloseButton,
   Text,
+  Box,
+  TableContainer,
 } from "@chakra-ui/react";
 import { EmptyView } from "..";
 import { StockResponse } from "../../services";
@@ -23,49 +25,49 @@ export const ListView = ({ stocks, handleRemove }: ListViewProps) => {
     <>
       {!stocks && <EmptyView view="list" />}
       {stocks && (
-        <Table variant="simple" mt={"0.5"} overflowX="scroll" display="block">
-          <Thead position="sticky" bg={bg}>
-            <Tr>
-              <Th>Symbol</Th>
-              <Th>Name</Th>
-              <Th>Exchange</Th>
-              <Th>Sector</Th>
-              <Th>Price</Th>
-              <Th>Difference</Th>
-              <Th>High</Th>
-              <Th>Low</Th>
-              <Th></Th>
-            </Tr>
-          </Thead>
-          <Tbody zIndex={"-5"}>
-            {stocks.map((stock) => (
-              <Tr key={stock.symbol}>
-                <Td>{stock.symbol}</Td>
-                <Td>{stock.name}</Td>
-                <Td>{stock.exchange}</Td>
-                <Td>{stock.sector}</Td>
-                <Td
-                  color={
-                    stock.globalQuote.changePercent.startsWith("-")
-                      ? "#F56565"
-                      : "#48BB78"
-                  }
-                >{`$${Number(stock.globalQuote.price).toFixed(2)}`}</Td>
-                <Td>{stock.globalQuote.changePercent}</Td>
-                <Td>{`$${Number(stock.globalQuote.high).toFixed(2)}`}</Td>
-                <Td>{`$${Number(stock.globalQuote.low).toFixed(2)}`}</Td>
-                <Td>
-                  <CloseButton onClick={() => handleRemove(stock.symbol)} />
-                </Td>
-              </Tr>
-            ))}
-            {/* {[...Array(50)].map(() => (
+        <TableContainer
+          overflowY="auto"
+          height={{ sm: "calc(100vh - 162px)", md: "calc(100vh - 124px)" }}
+        >
+          <Table variant="simple" mt={"0.5"}>
+            <Thead top={0} position="sticky" bg={bg}>
               <Tr>
-                <Td>50</Td>
+                <Th>Symbol</Th>
+                <Th>Name</Th>
+                <Th>Exchange</Th>
+                <Th>Sector</Th>
+                <Th>Price</Th>
+                <Th>Difference</Th>
+                <Th>High</Th>
+                <Th>Low</Th>
+                <Th></Th>
               </Tr>
-            ))} */}
-          </Tbody>
-        </Table>
+            </Thead>
+            <Tbody zIndex={"-5"}>
+              {stocks.map((stock) => (
+                <Tr key={stock.symbol}>
+                  <Td>{stock.symbol}</Td>
+                  <Td>{stock.name}</Td>
+                  <Td>{stock.exchange}</Td>
+                  <Td>{stock.sector}</Td>
+                  <Td
+                    color={
+                      stock.globalQuote.changePercent.startsWith("-")
+                        ? "#F56565"
+                        : "#48BB78"
+                    }
+                  >{`$${Number(stock.globalQuote.price).toFixed(2)}`}</Td>
+                  <Td>{stock.globalQuote.changePercent}</Td>
+                  <Td>{`$${Number(stock.globalQuote.high).toFixed(2)}`}</Td>
+                  <Td>{`$${Number(stock.globalQuote.low).toFixed(2)}`}</Td>
+                  <Td>
+                    <CloseButton onClick={() => handleRemove(stock.symbol)} />
+                  </Td>
+                </Tr>
+              ))}
+            </Tbody>
+          </Table>
+        </TableContainer>
       )}
     </>
   );
